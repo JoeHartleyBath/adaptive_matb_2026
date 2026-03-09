f4db2ce091d1e9e247b23aba2bd784d2d31a904c
Joe Hartley <jh3968@bath.ac.uk>
2026-03-09 16:16:29 +0000

    feat: add offline EEG windower (eeg_windower.py)

--- src/python/eeg/__init__.py
+++ src/python/eeg/__init__.py
@@ -3,11 +3,15 @@ from .eeg_stream_config import EegStreamConfig
 from .eeg_preprocessing_config import EegPreprocessingConfig
 from .eeg_preprocessor import EegPreprocessor
 from .eeg_filters import RealTimeFilter
+from .eeg_windower import WindowConfig, extract_windows, slice_block
 
 __all__ = [
-    "EegInlet", 
+    "EegInlet",
     "EegStreamConfig",
     "EegPreprocessingConfig",
     "EegPreprocessor",
-    "RealTimeFilter"
+    "RealTimeFilter",
+    "WindowConfig",
+    "extract_windows",
+    "slice_block",
 ]
